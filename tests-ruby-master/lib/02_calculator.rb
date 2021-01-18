840dcf1f048db976de69776e3a8ddd3ea7c1ebc4
AndyVapeur <77019333+AndyVapeur@users.noreply.github.com>
2021-01-18 17:46:14 +0100

    Add files via upload

---
+++ tests-ruby-master/lib/02_calculator.rb
@@ -0,0 +1,31 @@
+def add (a,b)
+ return a + b
+end
+
+def subtract (a,b)
+  return a - b
+end
+
+def sum (ary)
+  return ary.sum
+end
+
+def multiply (a,b)
+  return a * b
+end
+
+def power (a,b)
+  return a**b
+end
+
+def fact (n)
+  return Math.gamma(n+1)
+end
+
+
+
+
+
+
+
+
